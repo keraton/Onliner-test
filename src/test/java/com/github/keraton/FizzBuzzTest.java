c1e2d1cd7b03a69415ef568913a32eeada82703c
Bowie <keratonjava@gmail.com>
2019-01-14 02:06:45 +0100

    working

--- src/test/java/com/github/keraton/FizzBuzzTest.java
+++ src/test/java/com/github/keraton/FizzBuzzTest.java
@@ -1,7 +1,20 @@
 package com.github.keraton;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    @Test
+    public void should () {
+        // Given
+
+        // When
+
+        // Then
+        Assertions.assertEquals(1, 0);
+
+    }
 }
