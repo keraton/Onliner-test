a8bb1c46abeb291d1521bd165c736d734b4faaf3
Bowie <keratonjava@gmail.com>
2019-01-29 00:46:52 +0100

    test 10

--- src/main/java/com/github/keraton/RepeatedString.java
+++ src/main/java/com/github/keraton/RepeatedString.java
@@ -9,18 +9,21 @@ public class RepeatedString {
     static long repeatedString(String s, long n) {
         int count = 0;
         long length = Math.min(s.length(), n);
-        for (int i=0; i<length; i++) {
-            if (s.charAt(i) == 'a')
-                count++;
-        }
+        count = getCount(s, count, length);
 
         if (s.length() < n) {
-            for (int i=0; i < n - s.length(); i++) {
-                if(s.charAt(i) == 'a')
-                    count++;
-            }
+            long l = n - s.length();
+            count = getCount(s, count, l);
+        }
+        
 
+        return count;
+    }
 
+    private static int getCount(String s, int count, long length) {
+        for (int i = 0; i < length; i++) {
+            if (s.charAt(i) == 'a')
+                count++;
         }
         return count;
     }
--- src/test/java/com/github/keraton/RepeatedStringTest.java
+++ src/test/java/com/github/keraton/RepeatedStringTest.java
@@ -19,6 +19,7 @@ public class RepeatedStringTest {
         assertEquals(5, repeatedString("aaa", 5));
         assertEquals(3, repeatedString("aba", 4));
         assertEquals(4, repeatedString("aba", 6));
+        //assertEquals(7, repeatedString("aaa", 7));
 
 
     }
