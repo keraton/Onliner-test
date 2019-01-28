9d112d68e11134e288dc882766ad2a33124aa8ea
Bowie <keratonjava@gmail.com>
2019-01-29 00:24:27 +0100

    test 1

--- src/test/java/com/github/keraton/RepeatedStringTest.java
+++ src/test/java/com/github/keraton/RepeatedStringTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class RepeatedStringTest {
 
     @Test
-    public void should () {
+    public void should_repeat_string () {
         // When
         assertEquals(1, repeatedString("a", 1));
         assertEquals(2, repeatedString("aba", 3));
