0569b6307206a73527e5b4e7e2c5155f1f09f947
Bowie <keratonjava@gmail.com>
2019-01-30 00:48:52 +0100

    test 39

--- src/main/java/com/github/keraton/JumpingCloud.java
+++ src/main/java/com/github/keraton/JumpingCloud.java
@@ -3,8 +3,6 @@ package com.github.keraton;
 public class JumpingCloud {
 
     static int jumpingOnClouds(int... c) {
-        if (c.length > 3)
-            return 2;
-        return 1;
+        return c.length / 2;
     }
 }
