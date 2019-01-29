7db28b143c68694d21d9497f919449ed52f97e0f
Bowie <keratonjava@gmail.com>
2019-01-30 00:47:07 +0100

    test 37

--- src/test/java/com/github/keraton/JumpingCloudTest.java
+++ src/test/java/com/github/keraton/JumpingCloudTest.java
@@ -13,6 +13,7 @@ public class JumpingCloudTest {
         assertEquals(1, jumpingOnClouds(0, 0));
         assertEquals(1, jumpingOnClouds(0, 0, 0));
         assertEquals(2, jumpingOnClouds(0, 0, 0, 0));
+        assertEquals(2, jumpingOnClouds(0, 0, 0, 0));
 
     }
 
