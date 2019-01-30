53e09745486605ece245fb23ce3f5b60e10dae49
Bowie <keratonjava@gmail.com>
2019-01-30 09:50:34 +0100

    test 57

--- src/main/java/com/github/keraton/EqualizeArray.java
+++ src/main/java/com/github/keraton/EqualizeArray.java
@@ -6,13 +6,11 @@ import java.util.Map;
 public class EqualizeArray {
 
     static int equalizeArray(int... arr) {
-        int diff = 0;
         Map<Integer, Integer> alreadyIn = new HashMap<>();
         alreadyIn.put(arr[0], 1);
         for (int ar : arr) {
             if (!alreadyIn.containsKey(ar)) {
                 alreadyIn.put(ar, 1);
-                diff++;
             }
             else {
                 Integer counter = alreadyIn.get(ar);
