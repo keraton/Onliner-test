7e6feafc9c023f37d35e4cb0c8b439a1b65d115d
Bowie <keratonjava@gmail.com>
2019-02-01 19:23:43 +0100

    test 60

---
+++ src/main/java/com/github/keraton/TaumBday.java
@@ -0,0 +1,9 @@
+package com.github.keraton;
+
+public class TaumBday {
+
+
+    static int taumBday(int b, int w, int bc, int wc, int z) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/github/keraton/TaumBdayTest.java
@@ -0,0 +1,17 @@
+package com.github.keraton;
+
+import org.junit.Test;
+
+public class TaumBdayTest {
+
+    @Test
+    public void should () {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+}
