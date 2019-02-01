4f11f9b5ffe5f32f906498a379cb660c939f4839
Bowie <keratonjava@gmail.com>
2019-02-01 20:19:15 +0100

    test 82

--- src/main/java/com/github/keraton/TaumBday.java
+++ src/main/java/com/github/keraton/TaumBday.java
@@ -6,30 +6,20 @@ public class TaumBday {
     static long taumBday(int b, int w, int bc, int wc, int z) {
         long minResult = Long.MAX_VALUE;
         if (bc > wc) {
-            if (z > bc) {
-                minResult = (b*bc);
-            }
-            else {
-                for (int i=0; i<=b ; i++) {
-                    long tempResult = ((b-i)*wc) + (i*bc) + ((b-i)* z);
-                    if (tempResult < minResult && tempResult >0) {
-                        minResult = tempResult;
-                    }
+            for (int i=0; i<=b ; i++) {
+                long tempResult = ((b-i)*wc) + (i*bc) + ((b-i)* z);
+                if (tempResult < minResult && tempResult >0) {
+                    minResult = tempResult;
                 }
             }
 
             minResult += (w*wc);
         }
         else {
-            if (z > wc) {
-                minResult = (w * wc);
-            }
-            else {
-                for (int i = 0; i<=w ; i++) {
-                    long tempResult = ((w-i)*bc) + (i*wc) + ((w-i)* z);
-                    if (tempResult < minResult && tempResult >0) {
-                        minResult = tempResult;
-                    }
+            for (int i = 0; i<=w ; i++) {
+                long tempResult = ((w-i)*bc) + (i*wc) + ((w-i)* z);
+                if (tempResult < minResult && tempResult >0) {
+                    minResult = tempResult;
                 }
             }
             minResult += (b*bc);
