950623a95530315199a1823597776a5b47886be1
Bowie <keratonjava@gmail.com>
2019-02-08 00:02:36 +0100

    test 125

--- src/main/java/com/github/keraton/AcmTeam.java
+++ src/main/java/com/github/keraton/AcmTeam.java
@@ -6,6 +6,13 @@ public class AcmTeam {
         String topic1 = topics[0];
         String topic2 = topics[1];
 
+        int maxTopics = getMaxTopics(topic1, topic2);
+
+        int maxPersons = 1;
+        return new int[] {maxTopics, maxPersons};
+    }
+
+    private static int getMaxTopics(String topic1, String topic2) {
         int size = topic1.length();
         int maxTopics = 0;
         for (int i=0; i<size; i++) {
@@ -13,8 +20,6 @@ public class AcmTeam {
                 maxTopics++;
             }
         }
-
-        int maxPersons = 1;
-        return new int[] {maxTopics, maxPersons};
+        return maxTopics;
     }
 }
