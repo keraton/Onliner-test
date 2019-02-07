272ddffa11f0522eeadeb1393be1e1b810351cd9
Bowie <keratonjava@gmail.com>
2019-02-07 23:50:33 +0100

    test 118

---
+++ src/test/java/com/github/keraton/AcmTeamTest.java
@@ -0,0 +1,17 @@
+package com.github.keraton;
+
+import org.junit.Test;
+
+public class AcmTeamTest {
+
+    @Test
+    public void should () {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+}
