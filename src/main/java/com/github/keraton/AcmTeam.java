62762a5f4aa1f6d5dc4d53f47b00d3f00a3ed378
Bowie <keratonjava@gmail.com>
2019-02-07 23:58:50 +0100

    test 122

--- src/main/java/com/github/keraton/AcmTeam.java
+++ src/main/java/com/github/keraton/AcmTeam.java
@@ -2,8 +2,18 @@ package com.github.keraton;
 
 public class AcmTeam {
 
-    static int[] acmTeam(String... topic) {
-        int maxTopics = 1;
+    static int[] acmTeam(String... topics) {
+        String topic1 = topics[0];
+        String topic2 = topics[1];
+
+        int size = topic1.length();
+        int maxTopics = 0;
+        for (int i=0; i<size; i++) {
+            if (topic1.charAt(i) == topic2.charAt(i)) {
+                maxTopics++;
+            }
+        }
+
         int maxPersons = 1;
         return new int[] {maxTopics, maxPersons};
     }
--- src/test/java/com/github/keraton/AcmTeamTest.java
+++ src/test/java/com/github/keraton/AcmTeamTest.java
@@ -10,7 +10,7 @@ public class AcmTeamTest {
     @Test
     public void should () {
         assertThat(acmTeam("1", "1")).containsExactly(1, 1);
-        //assertThat(acmTeam("0", "0")).containsExactly(0, 1);
+        assertThat(acmTeam("11", "11")).containsExactly(2, 1);
 
     }
 
